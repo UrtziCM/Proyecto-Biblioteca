cc2e974a8f6f4b328807018f3f76f3149d0140ea
UrtziCM <urcrmedev@gmail.com>
2024-01-30 18:31:28 +0100

    FIX module-info.java now points to correct packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
-module org.example {
+module org.yxini {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
     requires jasperreports;
     requires java.sql;
     opens org.yxini to javafx.fxml;
+    opens org.yxini.controllers to javafx.fxml;
     exports org.yxini;
 }
