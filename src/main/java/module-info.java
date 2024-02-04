f686476219a457e8a4c091f04f0ad46a2bf2665e
UrtziCM <urcrmedev@gmail.com>
2024-02-04 18:20:05 +0100

    ADD module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module org.yxini {
     requires javafx.media;
     requires jasperreports;
     requires java.sql;
+
+    opens org.yxini.models to javafx.base;
     opens org.yxini to javafx.fxml;
     opens org.yxini.controllers to javafx.fxml;
     exports org.yxini;
